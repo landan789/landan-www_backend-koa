9fa47d441b3b5f5fae59a5c18fe9d5a5954f9f75
LANDAN <landanhsu@bpit888.com>
2019-02-18 10:20:24 +0800

    use function typing

--- src/middlewares/requestUndefinedPath.ts
+++ src/middlewares/requestUndefinedPath.ts
@@ -3,7 +3,7 @@ import { BodyHelper } from './../helpers';
 let oBodyHelper = new BodyHelper();
 
 function requestUndefinedPath () {
-  return async (oCtx:any, fNext:any ) => {
+  return async (oCtx:any, fNext:()=>{} ) => {
     let oBody = oBodyHelper.reponse('REQUEST_UNDEFINED_PATH');
     oCtx.response.body = oBody;
     await fNext();
