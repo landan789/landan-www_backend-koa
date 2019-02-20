a70c1f622770d1b5169d41f52472073c2bd38a22
LANDAN <landanhsu@bpit888.com>
2019-02-20 09:51:21 +0800

    Updated: RESPONSESE.ts

--- src/configs/RESPONSES.ts
+++ src/configs/RESPONSES.ts
@@ -1,8 +1,8 @@
 const REPONSEES:any = {
   'SUCCED_TO_SHOW_USER': { STATUS: 1, CODE: 0, MESSAGE: 'succed to show user'},
-  'IS_UNKNOWN_ERROR': { STATUS: -1, CODE: -1, MESSAGE: 'is unknown error'},
-  'REQUEST_UNDEFINED_PATH': { STATUS: -2, CODE: -1, MESSAGE: 'request undefined path'},
-  'FAIL_TO_SHOW_USER': { STATUS: -1, CODE: -3, MESSAGE: 'fail to show user'}
+  'IS_UNKNOWN_ERROR': { STATUS: -1, CODE: -0.01, MESSAGE: 'is unknown error'},
+  'REQUEST_UNDEFINED_PATH': { STATUS: -2, CODE: -0.02, MESSAGE: 'request undefined path'},
+  'FAIL_TO_SHOW_USER': { STATUS: -1, CODE: -1.01, MESSAGE: 'fail to show user'}
 };
 
 export default REPONSEES;
