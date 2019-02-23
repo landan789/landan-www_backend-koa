f475847e63a1372bedc9556bfdf7f2f8747b6a42
LANDAN <landanhsu@bpit888.com>
2019-02-23 20:52:38 +0800

    Modified, refactor

--- src/helpers/body.ts
+++ src/helpers/body.ts
@@ -4,7 +4,7 @@ import { REPONSES } from '../configs';
 class ResponseHelper {
   reponse (message:string, data?:[]) {
     let oBody = {
-      status: REPONSES[message].STATUS || -1,
+      result: REPONSES[message].result || -1,
       code: REPONSES[message].CODE || 0,
       message: REPONSES[message].MESSAGE || '',
       data: data || []
