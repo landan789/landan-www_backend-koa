3ea057ab0fab461c2f31345d23c39923bb71ad34
LANDAN <landanhsu@bpit888.com>
2019-02-24 12:17:17 +0800

    Modified, refactor

--- src/configs/RESPONSES.ts
+++ src/configs/RESPONSES.ts
@@ -1,8 +1,28 @@
 const REPONSEES:any = {
-  'IT_SUCCEDS_TO_SHOW_USER': { STATUS: 200, RESULT: 1, CODE: 0, MESSAGE: 'it succeds to show user'},
-  'IT_IS_UNKNOWN_ERROR': { STATUS: 500, RESULT: -1, CODE: -0.01, MESSAGE: 'it is unknown error'},
-  'IT_REQUESTS_UNDEFINED_PATH': { STATUS: 501, RESULT: -1, CODE: -0.02, MESSAGE: 'it requests undefined path'},
-  'IT_FAILS_TO_SHOW_USER': { STATUS: 500, RESULT: -1, CODE: -1.01, MESSAGE: 'it fails to show user'}
+  'IT_SUCCEDS_TO_SHOW_USER': { 
+    STATUS: 200, 
+    RESULT: 1, 
+    CODE: 0, 
+    MESSAGE: 'it succeds to show user'
+  },
+  'IT_IS_UNKNOWN_ERROR': { 
+    STATUS: 500, 
+    RESULT: -1, 
+    CODE: -0.01, 
+    MESSAGE: 'it is unknown error'
+  },
+  'IT_REQUESTS_UNDEFINED_PATH': { 
+    STATUS: 501, 
+    RESULT: -1, 
+    CODE: -0.02, 
+    MESSAGE: 'it requests undefined path'
+  },
+  'IT_FAILS_TO_SHOW_USER': { 
+    STATUS: 500, 
+    RESULT: -1, 
+    CODE: -1.01, 
+    MESSAGE: 'it fails to show user'
+  }
 };
 
 export default REPONSEES;
