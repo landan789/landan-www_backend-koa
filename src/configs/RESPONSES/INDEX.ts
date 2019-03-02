53c67b45d6921119358d834b70653ad110d43bac
landanhsu <landanhsu@bpit888.com>
2019-03-02 10:22:58 +0000

    Edited

--- src/configs/RESPONSES/INDEX.ts
+++ src/configs/RESPONSES/INDEX.ts
@@ -1,20 +1,24 @@
 const RESPONSEES: any = {
-  'SUCCED_TO_SHOW_USER': { 
+  'SUCCED_TO_SHOW_USER': {
+    STATUS: 200,
     RESULT: 1, 
     CODE: 0, 
     MESSAGE: 'succed to show user'
   },
   'UNKNON_ERROR': { 
+    STATUS: 500,
     RESULT: -1, 
     CODE: -1, 
     MESSAGE: 'unknown error'
   },
-  'REQUEST_NONEXISTENT_URI': { 
+  'REQUEST_NONEXISTENT_URI': {
+    STATUS: 501,
     RESULT: -2, 
     CODE: -1, 
     MESSAGE: 'request nonexistent URI'
   },
-  'FAIL_TO_SHOW_USER': { 
+  'FAIL_TO_SHOW_USER': {
+    STATUS: 500,
     RESULT: -1, 
     CODE: -3, 
     MESSAGE: 'fail to show user'
