c255f6d531bc8a0964b4450490cfb365fd3b8c8a
landanhsu <landanhsu@bpit888.com>
2019-03-02 09:17:39 +0000

    Edited, refactor middleware

--- src/configs/RESPONSES.ts
+++ src/configs/RESPONSES.ts
@@ -1,7 +1,7 @@
 const REPONSEES:any = {
   'SUCCED_TO_SHOW_USER': { status: 1, code: 0, message: 'succed to show user'},
   'UNKNON_ERROR': { status: -1, code: -1, message: 'unknown error'},
-  'REQUEST_UNDEFINED_PATH': { status: -2, code: -1, message: 'request undefined path'},
+  'REQUEST_NONEXISTENT_URI': { status: -2, code: -1, message: 'request nonexistent URI'},
   'FAIL_TO_SHOW_USER': { status: -1, code: -3, message: 'fail to show user'}
 };
 
