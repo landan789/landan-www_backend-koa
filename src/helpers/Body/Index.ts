4f99c1ab5bfd47f71c6a6872f3ea718825a74f10
landanhsu <landanhsu@bpit888.com>
2019-03-02 09:20:04 +0000

    Edited, refactor middleware

--- src/helpers/Body/Index.ts
+++ src/helpers/Body/Index.ts
@@ -7,6 +7,8 @@ class ResponseHelper {
       status: REPONSES[message].status || -1,
       code: REPONSES[message].code || 0,
       message: REPONSES[message].message || '',
+      jwt: '',
+      total_count: 0,
       data: data || []
     }
     return oBody;
